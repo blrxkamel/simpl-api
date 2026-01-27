c1e8f323990f070e3e2653bbe9225b27543c5b0a
blrxkamel <omranikamel567@gmail.com>
2026-01-27 23:34:40 +0100

    Update send.js

--- api/send.js
+++ api/send.js
@@ -1,1 +1,13 @@
+export default function handler(req, res) {
+  const { ip, port } = req.query;
 
+  if (!ip || !port) {
+    return res.status(400).json({ error: "ip or port missing" });
+  }
+
+  res.status(200).json({
+    ip,
+    port,
+    status: "ok"
+  });
+}
